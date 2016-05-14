7e71ac9a5faa6d963136592afc86f28a1bb47ada
Attila Večerek <veat24@gmail.com>
2016-05-14 15:00:01 +0200

    Bump version to 0.2.0

--- lib/css_compare/constants.rb
+++ lib/css_compare/constants.rb
@@ -1,5 +1,5 @@
 module CssCompare
   # The root directory of the Less2Sass source tree.
   ROOT_DIR = File.expand_path(File.join(__FILE__, '../../..'))
-  VERSION = '0.1.3'.freeze
+  VERSION = '0.2.0'.freeze
 end
